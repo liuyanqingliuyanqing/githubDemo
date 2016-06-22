607ed25ad0134f83ac34fa75594839d123d4f686
liuyanqingliuyanqing <1656102217@qq.com>
2016-06-22 10:11:41 +0800

    tree
    github

---
+++ githubDemo02/githubDemo02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubDemo02
+//
+//  Created by mac on 16/6/22.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubDemo02/githubDemo02/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  githubDemo02
+//
+//  Created by mac on 16/6/22.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
